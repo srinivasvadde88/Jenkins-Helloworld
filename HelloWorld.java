aa8abf2bad2529cd2c4dc0913f1bad5a18aa621f
Srinivas <srinivasvadde88@gmail.com>
2020-12-09 16:53:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Poll SCM"); 
+        System.out.println("*********************************Hello, World! Poll SCM**********************************"); 
     }
 }
